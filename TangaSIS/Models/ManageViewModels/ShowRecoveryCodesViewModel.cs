17b8e1730e3326dba3237fec013ef893cc8b24cd
Latif Adams <Latiftanga@hotmail.com>
2018-03-20 22:34:05 +0000

    Initial commit (Clean Project)

---
+++ TangaSIS/Models/ManageViewModels/ShowRecoveryCodesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TangaSIS.Models.ManageViewModels
+{
+    public class ShowRecoveryCodesViewModel
+    {
+        public string[] RecoveryCodes { get; set; }
+    }
+}
